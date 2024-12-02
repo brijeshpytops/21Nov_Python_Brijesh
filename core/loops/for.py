f4718c26254f265e88b65118bf353ea72206ae1e
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-12-02 13:30:17 +0530

    patterns

--- core/loops/for.py
+++ core/loops/for.py
@@ -29,8 +29,139 @@ for variable in sequence:
 # print(next(chs))
 # print(next(chs))
 
+
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num+1):
+#         print("*", end=" ")
+#     print()
+
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+
+# num = 5
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+
+# * * * * *
+# * * * * 
+# * * * 
+# * * 
+# * 
+# 1 - 5 = 6
+# 2 - 4 = 6
+# 3 - 3 = 6
+# 4 - 2 = 6
+# 5 - 1 = 6
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+2):
+#         print("*", end=" ")
+#     print()
+
+
+# - - - - * 
+# - - - * * 
+# - - * * * 
+# - * * * * 
+# * * * * *
+
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print(" ", end=" ")
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+
+# num = 5
+
+# for row in range(1, num+1):
+#     for col in range(1, row):
+#         print(" ", end=' ')
+#     for col in range(1, num-row+2):
+#         print("*", end=" ")
+#     print()
+
+
+num = 5
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print("*", end=" ")
+#     print()
+
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print(" ", end=" ")
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(" ", end=' ')
+#     for col in range(1, num-row+1):
+#         print("*", end=" ")
+#     print()
+
+# num = 5
+# g_var = 1
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(chr(g_var + 64), end=" ")
+#         g_var += 1
+#     print()
+
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         if col % 2 == 0:
+#             print("1", end=' ')
+#         else:
+#             print("0", end=' ')
+#     print()
+
 num = 5
 for row in range(1, num+1):
-    for col in range(1, num+1):
+    for col in range(1, num-row+1):
+        print(" ", end=" ")
+    for col in range(1, row+1):
+        print("*", end=' ')
+    for col in range(1, row):
+        print("*", end=' ')
+    print()
+
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(" ", end=' ')
+    for col in range(1, num-row+1):
+        print("*", end=" ")
+    for col in range(1, num-row):
         print("*", end=" ")
     print()
--- core/loops/patterns.py
+++ core/loops/patterns.py
@@ -9,3 +9,227 @@ for row in range(1, num+1):
     for col in range(1, num+1):
         print("*", end=" ")
     print()
+
+
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+
+num = 5
+
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print("*", end=' ')
+    print()
+
+# * * * * *
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+
+num = 5
+for row in range(1, num+1):
+    for col in range(1, num-row+2):
+        print("*", end=" ")
+    print()
+
+
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+# * * * * *
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print(" ", end=" ")
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+# * * * * *
+#   * * * * 
+#     * * * 
+#       * * 
+#         * 
+
+
+# num = 5
+
+# for row in range(1, num+1):
+#     for col in range(1, row):
+#         print(" ", end=' ')
+#     for col in range(1, num-row+2):
+#         print("*", end=" ")
+#     print()
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print("*", end=" ")
+#     print()
+
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+# * * * * *
+#   * * * * 
+#     * * * 
+#       * * 
+#         * 
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print(" ", end=" ")
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     print()
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(" ", end=' ')
+#     for col in range(1, num-row+1):
+#         print("*", end=" ")
+#     print()
+
+
+# 0
+# 1 1
+# 0 0 0
+# 1 1 1 1
+# 0 0 0 0 0
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         if row % 2 == 0:
+#             print("1", end=' ')
+#         else:
+#             print("0", end=' ')
+#     print()
+
+
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+
+num = 5
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(row, end=" ")
+    print()
+
+
+# A
+# B B
+# C C C
+# D D D D
+# E E E E E
+
+
+num = 5
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(chr(row + 64), end=" ")
+    print()
+
+
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+
+
+num = 5
+g_var = 1
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(g_var, end=" ")
+        g_var += 1
+    print()
+
+# A
+# B C
+# D E F
+# G H I J
+# K L M N O
+
+num = 5
+g_var = 1
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        print(chr(g_var + 64), end=" ")
+        g_var += 1
+    print()
+
+
+# 0
+# 0 1
+# 0 1 0
+# 0 1 0 1
+# 0 1 0 1 0
+
+for row in range(1, num+1):
+    for col in range(1, row+1):
+        if col % 2 == 0:
+            print("1", end=' ')
+        else:
+            print("0", end=' ')
+    print()
+
+
+
+#         * 
+#       * * * 
+#     * * * * *
+#   * * * * * * * 
+# * * * * * * * * * 
+#   * * * * * * * 
+#     * * * * * 
+#       * * *
+#         * 
+
+
+# num = 5
+# for row in range(1, num+1):
+#     for col in range(1, num-row+1):
+#         print(" ", end=" ")
+#     for col in range(1, row+1):
+#         print("*", end=' ')
+#     for col in range(1, row):
+#         print("*", end=' ')
+#     print()
+
+# for row in range(1, num+1):
+#     for col in range(1, row+1):
+#         print(" ", end=' ')
+#     for col in range(1, num-row+1):
+#         print("*", end=" ")
+#     for col in range(1, num-row):
+#         print("*", end=" ")
+#     print()
